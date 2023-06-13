16c7057bb1cc68c5bb9913fe8976150eb32f7bcb
Ilias Zampetakis <eliaszabetakis@hotmail.com>
2023-06-13 17:14:22 +0300

    message

--- app.py
+++ app.py
@@ -172,6 +172,7 @@ WHERE kimai2_users_teams.teamlead=1;
         for i in range(len(tabs)):
             with tabs[i]:
                 dftest=df1[df1['project_name']==str(tabs[i])]
+                st.write(tabs[i])
                 st.write(dftest)
                 sql2=""" 
                         SELECT kimai2_projects_teams.*,kimai2_projects.name,kimai2_projects.visible,kimai2_projects.time_budget,kimai2_projects.budget,kimai2_users_teams.user_id,kimai2_users_teams.teamlead,
