92f362a7d9e1bb1bdc3cfce4663a3f73fc0e7bcd
Ilias Zampetakis <eliaszabetakis@hotmail.com>
2023-06-13 14:55:48 +0300

    message

--- app.py
+++ app.py
@@ -90,8 +90,7 @@ WHERE kimai2_users_teams.teamlead=1;
     # st.write(columnames)
         dfdata=pd.DataFrame(rows,columns=columnames)
         st.write("All Data from Query",dfdata)
-        st.write('Your birthday is:', startdate)
-        st.write('Your birthday is:', enddate)
+   
         # Load the tips dataset from Plotly
 
 
@@ -141,6 +140,20 @@ WHERE kimai2_users_teams.teamlead=1;
                     title="Project Manager: "+str(selected_option)+" - Hourly projects duaration")
         st.plotly_chart(fig2)
 
+
+        tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
+
+        with tab1:
+            st.header("A cat")
+            st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+
+        with tab2:
+            st.header("A dog")
+            st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+
+        with tab3:
+            st.header("An owl")
+            st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
 
 
 
