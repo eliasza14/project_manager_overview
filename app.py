fc59dc3f76eb12ab141a11634ef2ca104bcd3bed
Ilias Zampetakis <eliaszabetakis@hotmail.com>
2023-06-13 15:30:32 +0300

    message

--- app.py
+++ app.py
@@ -172,24 +172,24 @@ WHERE kimai2_users_teams.teamlead=1;
                 st.header(tab_content["title"])
                 st.write(tab_content["content"])
                 
-        tabs = st.tabs(["metrics", "plots", "reports"])
+        # tabs = st.tabs(["metrics", "plots", "reports"])
 
             
 
 
-        tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
+        # tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
 
-        with tab1:
-            st.header("A cat")
-            st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+        # with tab1:
+        #     st.header("A cat")
+        #     st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
 
-        with tab2:
-            st.header("A dog")
-            st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+        # with tab2:
+        #     st.header("A dog")
+        #     st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
 
-        with tab3:
-            st.header("An owl")
-            st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
+        # with tab3:
+        #     st.header("An owl")
+        #     st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
 
 
 
