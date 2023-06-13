bf6991d4e8355635121f791833ac63d4553b1fbf
Ilias Zampetakis <eliaszabetakis@hotmail.com>
2023-06-13 17:25:02 +0300

    message

--- app.py
+++ app.py
@@ -172,7 +172,7 @@ WHERE kimai2_users_teams.teamlead=1;
         for i in range(len(tabs)):
             with tabs[i]:
                 dftest=df1[df1['project_name']==str(tablist[i])]
-                st.write(dftest['project_id'][0])
+                projectlist=dftest['project_id'].tolist()
                 sql2=""" 
                         SELECT kimai2_projects_teams.*,kimai2_projects.name,kimai2_projects.visible,kimai2_projects.time_budget,kimai2_projects.budget,kimai2_users_teams.user_id,kimai2_users_teams.teamlead,
                 (
@@ -187,7 +187,7 @@ WHERE kimai2_users_teams.teamlead=1;
                 INNER JOIN kimai2_timesheet ON kimai2_timesheet.user=kimai2_users_teams.user_id and kimai2_timesheet.project_id=kimai2_projects_teams.project_id 
                 INNER JOIN kimai2_users ON kimai2_users.id=kimai2_users_teams.user_id
                 INNER JOIN kimai2_teams ON kimai2_teams.id=kimai2_projects_teams.team_id
-                where kimai2_projects_teams.project_id="""+str(dftest['project_id'][0])+""" and kimai2_projects_teams.team_id="""+str(2)+"""
+                where kimai2_projects_teams.project_id="""+str(projectlist[0])+""" and kimai2_projects_teams.team_id="""+str(2)+"""
                 GROUP BY kimai2_users_teams.user_id;
                 
                 
