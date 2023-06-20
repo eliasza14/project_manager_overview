cc1fd5def78420a33134101384e4bbca07655266
Ilias Zampetakis <eliaszabetakis@hotmail.com>
2023-06-20 16:39:43 +0300

    message

--- pages/3_users-overview.py
+++ pages/3_users-overview.py
@@ -105,7 +105,7 @@ GROUP BY kimai2_users.alias; """
     df1 = dfdata2[dfdata2['alias'] == selected_option]
     # df1['duration']=df1['duration']/3600
     # df1.loc[:, 'duration'] = df1['duration'] / 3600
-    st.write(df1)
+    st.write(df1['duration'])
 
 
 
