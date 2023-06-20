573a7b762a79d7b2f18b095533ca06d8bb849799
Ilias Zampetakis <eliaszabetakis@hotmail.com>
2023-06-20 15:17:33 +0300

    message

--- pages/3_users-overview.py
+++ pages/3_users-overview.py
@@ -22,6 +22,25 @@ def main():
     conn = init_connection()
     st.set_page_config(page_title="User Overview")
     st.title("User Overview")
+    sql = """SELECT kimai2_projects.name,kimai2_users.alias,SUM(kimai2_timesheet.duration) as duration ,MIN(kimai2_timesheet.start_time) as startime,MAX(kimai2_timesheet.start_time) as lasttime,kimai2_projects.visible
+FROM `kimai2_timesheet`
+INNER JOIN `kimai2_users` ON kimai2_users.id=kimai2_timesheet.user
+INNER JOIN `kimai2_projects` ON kimai2_projects.id=kimai2_timesheet.project_id
+GROUP BY kimai2_users.alias,kimai2_projects.name,kimai2_projects.visible;"""
+
+    sql2 ="""SELECT kimai2_users.alias,SUM(kimai2_timesheet.duration) as duration 
+FROM `kimai2_timesheet`
+INNER JOIN `kimai2_users` ON kimai2_users.id=kimai2_timesheet.user
+GROUP BY kimai2_users.alias; """
+
+    
+
+    rows,columnames = run_query(conn,sql)
+
+# st.write(columnames)
+    dfdata=pd.DataFrame(rows,columns=columnames)
+    st.write("All Data from Query",dfdata)
+
 
 
 
