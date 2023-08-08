c36b4700e88964802363bf286e8c6a5752107477
A-PALIOS <paliosadonis6@gmail.com>
2023-08-08 15:53:32 +0300

    message

--- pages/4_Project-overview.py
+++ pages/4_Project-overview.py
@@ -174,180 +174,180 @@ def main():
         st.write(dfgroup2)
 
 
-            # Create the pie chart using Plotly Express
-        fig = px.pie(dfgroup2, values=countlist, names='alias', hover_data=[dfgroup2['name']],labels={'alias':'User',
-                                                                                                            'values':'Project count',
-                                                                                                            'name':'Projects'})
-        fig.update_traces(textposition='inside', textinfo='label+value')
+    #         # Create the pie chart using Plotly Express
+    #     fig = px.pie(dfgroup2, values=countlist, names='alias', hover_data=[dfgroup2['name']],labels={'alias':'User',
+    #                                                                                                         'values':'Project count',
+    #                                                                                                         'name':'Projects'})
+    #     fig.update_traces(textposition='inside', textinfo='label+value')
 
-        # Display the pie chart
+    #     # Display the pie chart
 
 
-        st.title("User Overview")
-        st.plotly_chart(fig)
+    #     st.title("User Overview")
+    #     st.plotly_chart(fig)
 
-        rows,columnames = run_query(conn,sql2)
+    #     rows,columnames = run_query(conn,sql2)
 
-        # st.write(columnames)
-        dfdata2=pd.DataFrame(rows,columns=columnames)
-        dfdata2=dfdata2[dfdata2['alias']!='ADMINISTRATOR']
-        st.write("All Data from Filter data2",dfdata2)
-        dfdata2.loc[:, 'duration'] = dfdata2['duration'] // 3600
+    #     # st.write(columnames)
+    #     dfdata2=pd.DataFrame(rows,columns=columnames)
+    #     dfdata2=dfdata2[dfdata2['alias']!='ADMINISTRATOR']
+    #     st.write("All Data from Filter data2",dfdata2)
+    #     dfdata2.loc[:, 'duration'] = dfdata2['duration'] // 3600
         
-        dfdata2=dfdata2.sort_values('duration', ascending=False)
+    #     dfdata2=dfdata2.sort_values('duration', ascending=False)
         
-        st.write("All Data from Query",dfdata2)
+    #     st.write("All Data from Query",dfdata2)
 
 
-        fig2 = go.Figure(go.Bar(
-        x=dfdata2['duration'].tolist(),
-        y=dfdata2['alias'].tolist(),
-        orientation='h'))
+    #     fig2 = go.Figure(go.Bar(
+    #     x=dfdata2['duration'].tolist(),
+    #     y=dfdata2['alias'].tolist(),
+    #     orientation='h'))
 
-        fig2.update_layout(title="Users Total Working Hours",yaxis=dict(autorange="reversed"))
-        st.plotly_chart(fig2)
+    #     fig2.update_layout(title="Users Total Working Hours",yaxis=dict(autorange="reversed"))
+    #     st.plotly_chart(fig2)
 
-        st.title("Select User from the below list:")
-            # List of options for the dropdown menu
-        optionlist =dfdata2['alias'].tolist()
-        options = optionlist
+    #     st.title("Select User from the below list:")
+    #         # List of options for the dropdown menu
+    #     optionlist =dfdata2['alias'].tolist()
+    #     options = optionlist
 
-        # Display the dropdown menu
-        selected_option = st.selectbox('Choose User', options)
-        df1 = dfdata2[dfdata2['alias'] == selected_option]
-        # df1['duration']=df1['duration']/3600
-        # df1.loc[:, 'duration'] = df1['duration'] / 3600
-        first_duration_value = df1['duration'].iloc[0]
-        first_alias_value = df1['alias'].iloc[0]
-        text="**Total** **duration** **is:** **"+str(first_duration_value)+"** **Hours** **\u23F0** "
-        #st.write(first_alias_value)
-        #st.markdown(text)
-        st.title(text)
-        userdf = dfdata.loc[dfdata['alias']==first_alias_value]
-        userdf['cost']=userdf['value'].astype(float)*userdf['duration'].astype(float)
-        st.write(userdf)
+    #     # Display the dropdown menu
+    #     selected_option = st.selectbox('Choose User', options)
+    #     df1 = dfdata2[dfdata2['alias'] == selected_option]
+    #     # df1['duration']=df1['duration']/3600
+    #     # df1.loc[:, 'duration'] = df1['duration'] / 3600
+    #     first_duration_value = df1['duration'].iloc[0]
+    #     first_alias_value = df1['alias'].iloc[0]
+    #     text="**Total** **duration** **is:** **"+str(first_duration_value)+"** **Hours** **\u23F0** "
+    #     #st.write(first_alias_value)
+    #     #st.markdown(text)
+    #     st.title(text)
+    #     userdf = dfdata.loc[dfdata['alias']==first_alias_value]
+    #     userdf['cost']=userdf['value'].astype(float)*userdf['duration'].astype(float)
+    #     st.write(userdf)
 
-        fig3 = px.pie(userdf, values='duration', names='name',
-                title='User Project durations',
-                hover_data=['duration'], labels={'duration':'duration'})
-        fig3.update_traces(textposition='inside', textinfo='percent+label')
-        st.plotly_chart(fig3)
-
-
-        st.write('Rate per Project for selected user')
-
-        figrate= px.bar(userdf, y='cost', x='name', text_auto='.2s',title="Project Manager: "+str(selected_option)+" - Hourly projects duaration")
-        figrate.update_layout(barmode='stack', xaxis={'categoryorder': 'total descending'})
-
-        st.plotly_chart(figrate)
+    #     fig3 = px.pie(userdf, values='duration', names='name',
+    #             title='User Project durations',
+    #             hover_data=['duration'], labels={'duration':'duration'})
+    #     fig3.update_traces(textposition='inside', textinfo='percent+label')
+    #     st.plotly_chart(fig3)
 
 
+    #     st.write('Rate per Project for selected user')
+
+    #     figrate= px.bar(userdf, y='cost', x='name', text_auto='.2s',title="Project Manager: "+str(selected_option)+" - Hourly projects duaration")
+    #     figrate.update_layout(barmode='stack', xaxis={'categoryorder': 'total descending'})
+
+    #     st.plotly_chart(figrate)
 
 
-        st.title("Select Project from the below list")
+
+
+    #     st.title("Select Project from the below list")
         
-        optionlist =userdf['name'].tolist()
-        options2 = optionlist
-        selected_option = st.selectbox('Choose Project', options2)
-        df2 = userdf[userdf['name'] == selected_option]
-        #st.write(df2)
-        first_name_value2 = df2['name'].iloc[0]
-        first_alias_value2 = df2['alias'].iloc[0]
+    #     optionlist =userdf['name'].tolist()
+    #     options2 = optionlist
+    #     selected_option = st.selectbox('Choose Project', options2)
+    #     df2 = userdf[userdf['name'] == selected_option]
+    #     #st.write(df2)
+    #     first_name_value2 = df2['name'].iloc[0]
+    #     first_alias_value2 = df2['alias'].iloc[0]
 
+    # #     sql3=""" SELECT kimai2_users.alias,kimai2_projects.name,kimai2_timesheet.start_time,kimai2_timesheet.duration
+    # # FROM `kimai2_timesheet`
+    # # INNER JOIN `kimai2_users` ON kimai2_users.id=kimai2_timesheet.user
+    # # Inner JOIN `kimai2_projects` ON kimai2_projects.id=kimai2_timesheet.project_id
+    # # WHERE kimai2_users.alias="""+str(first_alias_value2)+""" and kimai2_projects.name="""+str(first_name_value2)+""";"""
+        
     #     sql3=""" SELECT kimai2_users.alias,kimai2_projects.name,kimai2_timesheet.start_time,kimai2_timesheet.duration
     # FROM `kimai2_timesheet`
     # INNER JOIN `kimai2_users` ON kimai2_users.id=kimai2_timesheet.user
     # Inner JOIN `kimai2_projects` ON kimai2_projects.id=kimai2_timesheet.project_id
-    # WHERE kimai2_users.alias="""+str(first_alias_value2)+""" and kimai2_projects.name="""+str(first_name_value2)+""";"""
-        
-        sql3=""" SELECT kimai2_users.alias,kimai2_projects.name,kimai2_timesheet.start_time,kimai2_timesheet.duration
-    FROM `kimai2_timesheet`
-    INNER JOIN `kimai2_users` ON kimai2_users.id=kimai2_timesheet.user
-    Inner JOIN `kimai2_projects` ON kimai2_projects.id=kimai2_timesheet.project_id
-    WHERE kimai2_users.alias='"""+str(first_alias_value2)+"""' AND kimai2_projects.name='"""+str(first_name_value2)+"""';"""
+    # WHERE kimai2_users.alias='"""+str(first_alias_value2)+"""' AND kimai2_projects.name='"""+str(first_name_value2)+"""';"""
 
-        rows,columnames = run_query(conn,sql3)
+    #     rows,columnames = run_query(conn,sql3)
 
-        # st.write(columnames)
-        dfdata3=pd.DataFrame(rows,columns=columnames)
-        st.write("All Data from Query",dfdata3)
+    #     # st.write(columnames)
+    #     dfdata3=pd.DataFrame(rows,columns=columnames)
+    #     st.write("All Data from Query",dfdata3)
         
-        # Convert 'start_time' column to datetime
-        dfdata3['start_time'] = pd.to_datetime(dfdata3['start_time'])
-        dfdata3['year'] = dfdata3['start_time'].dt.year
+    #     # Convert 'start_time' column to datetime
+    #     dfdata3['start_time'] = pd.to_datetime(dfdata3['start_time'])
+    #     dfdata3['year'] = dfdata3['start_time'].dt.year
     
-    # Apply the formatting function to the 'Year' column
-        dfdata3['year'] = dfdata3['year'].apply(format_year)
-        # dfdata3['year']=dfdata3['year'].str.replace(',', '').astype(int)
-        st.write(dfdata3['year'].dtype)
+    # # Apply the formatting function to the 'Year' column
+    #     dfdata3['year'] = dfdata3['year'].apply(format_year)
+    #     # dfdata3['year']=dfdata3['year'].str.replace(',', '').astype(int)
+    #     st.write(dfdata3['year'].dtype)
 
-        # Extract month from 'start_time' column
-        dfdata3['month'] = dfdata3['start_time'].dt.month
+    #     # Extract month from 'start_time' column
+    #     dfdata3['month'] = dfdata3['start_time'].dt.month
 
-        # Convert 'duration' column to numeric
-        dfdata3['duration'] = (dfdata3['duration'] / 3600).astype(int)
-        st.write("After Preprocessing Data from Query",dfdata3)
+    #     # Convert 'duration' column to numeric
+    #     dfdata3['duration'] = (dfdata3['duration'] / 3600).astype(int)
+    #     st.write("After Preprocessing Data from Query",dfdata3)
 
 
 
-        # Group by month and calculate total duration
-        dfdata3group = dfdata3.groupby('month')['duration'].sum().reset_index()
+    #     # Group by month and calculate total duration
+    #     dfdata3group = dfdata3.groupby('month')['duration'].sum().reset_index()
 
-        # Create all 12 months
-        all_months = list(range(1, 13))
+    #     # Create all 12 months
+    #     all_months = list(range(1, 13))
 
-        # Add missing months to the DataFrame with duration set to 0
-        dfdata3group = dfdata3group.merge(pd.DataFrame({'month': all_months}), how='right')
+    #     # Add missing months to the DataFrame with duration set to 0
+    #     dfdata3group = dfdata3group.merge(pd.DataFrame({'month': all_months}), how='right')
 
-        # Sort the DataFrame by month
-        dfdata3group = dfdata3group.sort_values('month')
+    #     # Sort the DataFrame by month
+    #     dfdata3group = dfdata3group.sort_values('month')
 
-        # Fill missing duration values with 0
-        dfdata3group['duration'] = dfdata3group['duration'].fillna(0)
+    #     # Fill missing duration values with 0
+    #     dfdata3group['duration'] = dfdata3group['duration'].fillna(0)
 
-        # Get the name of each month
-        dfdata3group['month_name'] = dfdata3group['month'].apply(lambda x: calendar.month_name[x])
+    #     # Get the name of each month
+    #     dfdata3group['month_name'] = dfdata3group['month'].apply(lambda x: calendar.month_name[x])
 
-        # Create line chart
-        fig = go.Figure()
+    #     # Create line chart
+    #     fig = go.Figure()
 
-        # Add line trace
-        fig.add_trace(go.Scatter(
-            x=dfdata3group['month_name'],
-            y=dfdata3group['duration'],
-            mode='lines',
-            name='Duration'
-        ))
+    #     # Add line trace
+    #     fig.add_trace(go.Scatter(
+    #         x=dfdata3group['month_name'],
+    #         y=dfdata3group['duration'],
+    #         mode='lines',
+    #         name='Duration'
+    #     ))
 
-        # Identify the months with non-zero sum duration
-        non_zero_months = dfdata3group[dfdata3group['duration'] > 0]
+    #     # Identify the months with non-zero sum duration
+    #     non_zero_months = dfdata3group[dfdata3group['duration'] > 0]
 
-        # Add dots for non-zero months
-        fig.add_trace(go.Scatter(
-            x=non_zero_months['month_name'],
-            y=non_zero_months['duration'],
-            mode='markers',
-            marker=dict(
-                color='green',
-                size=10,
-                symbol='circle',
-                line=dict(
-                    width=2,
-                    color='green'
-                )
-            ),
-            name='Non-Zero Months'
-        ))
+    #     # Add dots for non-zero months
+    #     fig.add_trace(go.Scatter(
+    #         x=non_zero_months['month_name'],
+    #         y=non_zero_months['duration'],
+    #         mode='markers',
+    #         marker=dict(
+    #             color='green',
+    #             size=10,
+    #             symbol='circle',
+    #             line=dict(
+    #                 width=2,
+    #                 color='green'
+    #             )
+    #         ),
+    #         name='Non-Zero Months'
+    #     ))
 
-        # Set axis labels and chart title
-        fig.update_layout(
-            xaxis_title='Months',
-            yaxis_title='Total Duration of Project:'+first_name_value2+" in Hours",
-            title='Duration of the Project per Month'
-        )
+    #     # Set axis labels and chart title
+    #     fig.update_layout(
+    #         xaxis_title='Months',
+    #         yaxis_title='Total Duration of Project:'+first_name_value2+" in Hours",
+    #         title='Duration of the Project per Month'
+    #     )
 
-        # Display the chart
-        st.plotly_chart(fig)
+    #     # Display the chart
+    #     st.plotly_chart(fig)
 
 
 
