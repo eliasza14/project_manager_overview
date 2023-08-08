0ce23e5b10722db8ca41e8a1e7f293745105470f
Ilias Zampetakis <eliaszabetakis@hotmail.com>
2023-08-08 16:51:15 +0300

    message

--- pages/4_Project-overview.py
+++ pages/4_Project-overview.py
@@ -90,7 +90,7 @@ def main():
         sql = f"""
         SELECT kimai2_projects.name, kimai2_users.alias, SUM(kimai2_timesheet.duration) as duration,
             MIN(kimai2_timesheet.start_time) as startime, MAX(kimai2_timesheet.start_time) as lasttime,
-            kimai2_projects.visible, kimai2_user_preferences.name as rate, kimai2_user_preferences.value
+            kimai2_projects.visible, kimai2_user_preferences.name as rate, kimai2_user_preferences.value,kimai2_users.enabled as active_user
         FROM `kimai2_timesheet`
         INNER JOIN `kimai2_users` ON kimai2_users.id=kimai2_timesheet.user
         INNER JOIN `kimai2_projects` ON kimai2_projects.id=kimai2_timesheet.project_id
