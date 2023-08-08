ab5fa5bdd352acf4727edff9b9b86dbd81f49f41
Ilias Zampetakis <eliaszabetakis@hotmail.com>
2023-08-08 12:12:54 +0300

    message

--- pages/3_users-overview.py
+++ pages/3_users-overview.py
@@ -90,6 +90,7 @@ def main():
     # st.write(columnames)
         dfdata=pd.DataFrame(rows,columns=columnames)
         st.write("All Data from Query",dfdata)
+        dfdata=dfdata[dfdata['alias']!='ADMINISTRATOR']
         dfdata.loc[:, 'duration'] = dfdata['duration'] // 3600
 
         dfgroup=dfdata.groupby(['alias'])['name'].count()
