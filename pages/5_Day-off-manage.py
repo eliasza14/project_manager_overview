084f8e4c22c07bfd6576b367b71b03d036a9f030
Ilias Zampetakis <eliaszabetakis@hotmail.com>
2023-08-31 13:19:36 +0300

    message

---
+++ pages/5_Day-off-manage.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import pandas as pd
+import mysql.connector
+import datetime
+from streamlit import session_state
+import plotly.express as px
+import plotly.graph_objects as go
+from datetime import timedelta
+import calendar 
+from streamlit.components.v1 import html
+
+from html_shortcuts import *
+def init_connection():
+    return mysql.connector.connect(**st.secrets["mysql"])
+
+def run_query(conn,query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        columnsnames=cur.column_names
+        return cur.fetchall(),columnsnames
+    
+def main():
+
+    conn = init_connection()
+    st.set_page_config(page_title="DayOff Management")
+
+    st.title("DayOff Management")
+
+
+
+
+if __name__ == '__main__':
+    main()
