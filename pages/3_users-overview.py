cdd7803ea0fc23429597d807fd84637b4feabae8
Ippokratis Geranakis <igeran@cmt.local>
2025-01-29 13:49:01 +0200

    message

--- pages/3_users-overview.py
+++ pages/3_users-overview.py
@@ -275,6 +275,11 @@ def main():
     
     # Apply the formatting function to the 'Year' column
         dfdata3['year'] = dfdata3['year'].apply(format_year)
+
+        options = dfdata3['year'].tolist()
+
+        # Display the dropdown menu
+        selected_option = st.selectbox('Choose Year', options)
         # dfdata3['year']=dfdata3['year'].str.replace(',', '').astype(int)
         st.write(dfdata3['year'].dtype)
 
