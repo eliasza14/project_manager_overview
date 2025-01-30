d295690be388a0d6d73dff36c9a65579ea1f0f81
Ippokratis Geranakis <igeran@cmt.local>
2025-01-30 12:14:54 +0200

    message

--- pages/4_Project-overview.py
+++ pages/4_Project-overview.py
@@ -211,7 +211,7 @@ def main():
             with col2:
                 st.write(df1)
                 val=len(df1['alias'][dfdata['enabled']==1].unique())
-                val2 = df1['alias'][dfdata['enabled']==1].unique()
+                val2 = (df1['value'] * df1['duration']).sum()
                 st.write(val2)
                 html_content3 = html_button3(js_code,val)
                 html(html_content3, height=250)
